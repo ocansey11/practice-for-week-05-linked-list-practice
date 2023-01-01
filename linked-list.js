588d8e6d41d564eb70a1870cfa6108b08871e746
kevin ocansey <kevinocansey11@gmail.com>
2023-01-01 13:24:21 +0000

    constructor + #addTohead method completed

--- linked-list.js
+++ linked-list.js
@@ -9,18 +9,37 @@ class LinkedList {
   constructor() {
     // Your code here
     this.head = null
+    // this.tail = null
     this.length = 0
   }
 
   addToHead(val) {
     // Your code here
-    const newNode = new LinkedListNode(val, this.head)
+    const newNode = new LinkedListNode(val)
+    newNode.next = this.head
     this.head = newNode
     this.length++
+
+    if(!this.tail){
+      this.tail = this.head
+    }
   }
 
   addToTail(val) {
     // Your code here
+    const newNode1 =  new LinkedListNode(val)
+    let current  = this.head
+
+    if(this.head == null){
+      this.head =  newNode1
+    }
+    else{
+      while (current.next) {
+        current = current.next
+      }
+      current.next = newNode1
+    }
+    this.length++
   }
 
   // You can use this function to help debug
