ba61b19b5f03116be16da558915f25a7aa201a02
kevin ocansey <kevinocansey11@gmail.com>
2023-01-02 12:34:29 +0000

    #addToTail + #addToHead method completed

--- doubly-linked-list.js
+++ doubly-linked-list.js
@@ -9,14 +9,45 @@ class DoublyLinkedListNode {
 class DoublyLinkedList {
   constructor() {
     // Your code here
+    this.head =  null
+    this.tail = null
+    this.length = 0
   }
 
   addToHead(val) {
     // Your code here
+    const newNode =  new DoublyLinkedListNode(val)
+    if(!this.head){
+      this.tail = newNode
+    }
+
+    if(this.head == null  && this.tail){
+      this.head = newNode
+      this.head.next = this.tail
+      this.tail.prev = this.head
+    }
+    newNode.next = this.head
+    this.head.prev = newNode
+    this.head = newNode
+
+    this.length++
   }
 
   addToTail(val) {
     // Your code here
+    const newNode =  new DoublyLinkedListNode(val)
+    if(!this.head && !this.tail){
+      this.head = newNode
+    }
+    if(this.head && !this.tail){
+      this.tail = newNode
+      this.tail.prev = this.head
+    }
+    this.tail.next =  newNode
+    newNode.prev  = this.tail
+    this.tail =  newNode
+
+    this.length++
   }
 
   // You can use this function to help debug
